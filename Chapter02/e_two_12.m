3d06216a721f98d7fcf3e8c40ca51a524ab97265
mayyzym <mayyzym@gmail.com>
2019-02-20 17:32:42 +0800

    Calculation of complex Numbers

---
+++ Chapter02/e_two_12.m
@@ -0,0 +1,13 @@
+% 复数的实部、虚部、模和幅角的计算
+z=1+2*i;
+real_z=real(z);       % 实部
+imag_z=imag(z);       % 虚部
+mag_z=abs(z);         % 模
+angle_z0=angle(z);     % 幅角（弧度）
+angle_z1=angle(z)*180/pi;       % 幅角（角度）
+z
+real_z
+imag_z
+mag_z
+angle_z0
+angle_z1
